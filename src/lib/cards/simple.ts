e12ba84d390de2cc0a632f1ff342c4baf135a1f4
Wesam Haj-Ali <105243696+wesam4devops@users.noreply.github.com>
2023-03-31 17:58:44 -0400

    Update simple.ts

--- src/lib/cards/simple.ts
+++ src/lib/cards/simple.ts
@@ -15,41 +15,26 @@ export const populateCard = ({
       contentType: 'application/vnd.microsoft.card.adaptive',
       content: {
         type: 'AdaptiveCard',
-        $schema: 'http://adaptivecards.io/schemas/adaptive-card.json',
-        version: '1.3',
-        msteams: {
-          width: 'Full',
-        },
         body: [
           {
-            type: 'Container',
-            items: [
-              {
-                type: 'TextBlock',
-                text: title,
-                wrap: true,
-                size: 'Large',
-                weight: 'Bolder',
-              },
-            ],
-            style: color,
-            bleed: true,
-          },
-          {
             type: 'TextBlock',
-            text: text,
-            wrap: true,
-            height: 'stretch',
-          },
-          {
-            type: 'mention',
-            text: `<at>DevOps - test</at>`,
-            mentioned: {
-              id: `19:3c0d68f48f3f4aa0bd97eb0db82cadf5`,
-              name: `DevOps - test`
-            }
-          },
+            text: 'hi <at>DevOps - test</at>'
+          }
         ],
+        $schema: 'http://adaptivecards.io/schemas/adaptive-card.json',
+        version: '1.0',
+        msteams: {
+          entities: [
+            {
+              type: 'mention',
+              text: '<at>DevOps - test</at>',
+              mentioned: {
+                id: '19:3c0d68f48f3f4aa0bd97eb0db82cadf5',
+                name: 'DevOps - test'
+              }
+            }
+          ]
+        },
       },
     },
   ],
